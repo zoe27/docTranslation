8086d9ddc33d5f5cfe2d08e2a5a0385dfda72bdf
zoe <zoe27@126.com>
2025-07-12 11:29:16 +0800

    再次尝试修复404问题

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
+    // ✅ 明确设置项目结构以确保 Vercel 能识别
+    experimental: {
+      appDir: false // ✅ 明确禁用 appDir 模式（你用的是 pages 目录）
+    }
   }
   
   module.exports = nextConfig
